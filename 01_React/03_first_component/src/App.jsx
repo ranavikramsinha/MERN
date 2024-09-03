18fb00cff3e90f2305094f4aa70f61eece661ef1
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-09-03 23:49:50 +0530

    class and function components

---
+++ 01_React/03_first_component/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Heading from './components/heading'
+import Paragraph from './components/Paragraph'
+import Button from './components/Button'
+
+function App() {
+
+  return (
+    <>
+      <Heading></Heading>
+      <Paragraph></Paragraph>
+      <Button></Button>
+    </>
+  )
+}
+
+export default App
---
+++ 01_React/03_first_component/src/components/Paragraph.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './Paragraph.css'
+
+const Paragraph = () => {
+        return <p className ='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi animi reprehenderit quidem voluptatum eligendi? Eaque aspernatur quisquam velit hic vero aliquid nemo dolores, cum laborum sed pariatur. Minus ratione error iste nulla possimus reiciendis, blanditiis omnis animi quidem fugit velit laboriosam eos modi deserunt, sapiente quibusdam. Iste quia aliquid magni?</p>
+}
+
+export default Paragraph
