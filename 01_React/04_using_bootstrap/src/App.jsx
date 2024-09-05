87a72a2d83cf5360008531dff0d0d70655e26441
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-09-06 02:19:26 +0530

    bootstrap

---
+++ 01_React/04_using_bootstrap/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>PlaceHolder Text</div>
+      <button type="button" class="space-btn btn btn-primary">Primary</button>
+      <button type="button" class="space-btn btn btn-secondary">Secondary</button>
+      <button type="button" class="space-btn btn btn-success">Success</button>
+      <button type="button" class="space-btn btn btn-danger">Danger</button>
+      <button type="button" class="space-btn btn btn-warning">Warning</button>
+      <button type="button" class="space-btn btn btn-info">Info</button>
+      <button type="button" class="space-btn btn btn-light">Light</button>
+      <button type="button" class="space-btn btn btn-dark">Dark</button>
+
+      <button type="button" class="btn btn-link">Link</button>
+    </>
+  )
+}
+
+export default App
