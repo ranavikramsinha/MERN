ae7123cea9c8197adaec44dd285abfabb3d2f605
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-10-02 20:15:14 +0530

    more tailwind

--- 01_React/08_tailwind_css/01_vanilla/tailwind.config.js
+++ 01_React/08_tailwind_css/01_vanilla/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(122,124,255,1)',
+      }
+    },
   },
   plugins: [],
 }
