1a560a87262bec89074e7646bcc0cf650db30d6b
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-10-04 19:31:43 +0530

    passing children in react

--- 01_React/10_css_modules/src/App.jsx
+++ 01_React/10_css_modules/src/App.jsx
@@ -1,10 +1,16 @@
 import style from './App.module.css'
+import Container from './container'
 
 function App() {
 
   return (
     <>
-      <h1 className={`${style.heading} ${style.paragraph}`}>Hello World!</h1>
+      <Container>
+        <p>This is another content.</p>
+      </Container>
+      <Container>
+        <h1 className={`${style.heading} ${style.paragraph}`}>Hello World!</h1>
+      </Container>
     </>
   )
 }
