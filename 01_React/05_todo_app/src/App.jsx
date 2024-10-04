ba3db563486ed1f70646f456bf1df44bcac99471
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-10-04 13:45:38 +0530

    React.Fragment

--- 01_React/05_todo_app/src/App.jsx
+++ 01_React/05_todo_app/src/App.jsx
@@ -1,15 +1,19 @@
+import React from "react";
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import TodoItem from "./components/TodoItem";
 import TodoItems from "./components/TodoItems";
+
 function App() {
 
   return (
+    // <React.Fragment>
     <>
       <AppName />
       <AddTodo />
       <TodoItems />
     </>
+    // </React.Fragment>
   );
 }
 
