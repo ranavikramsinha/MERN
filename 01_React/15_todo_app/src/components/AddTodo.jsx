4d8df6a8648a0ea489f4e3602f1679d5a4638e26
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-10-18 03:09:30 +0530

    useRef

---
+++ 01_React/15_todo_app/src/components/AddTodo.jsx
@@ -0,0 +1,34 @@
+import Button from "./Button";
+
+const AddTodo = () => {
+  const addHandler = () => {
+    console.log(`addHandler Clicked`);
+  };
+
+  const textChangeHandler = (event) => {
+    console.log(event.target.value, event);
+  };
+
+  return (
+    <div className="container">
+      <div className="row todoList-row">
+        <div className="col-5">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Todo Here"
+            onChange={textChangeHandler}
+          ></input>
+        </div>
+        <div className="col-5">
+          <input type="date" className="form-control"></input>
+        </div>
+        <div className="col-2">
+          <Button btnType="success" btnText="Add" handler={addHandler} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddTodo;
---
+++ 01_React/15_todo_app/src/components/StateVsRef.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useRef } from 'react'
+
+const StateVsRef = () => {
+
+    console.log("Repaint StateVsRef Component");
+
+    const [stateClick, setStateClick] = useState(0);
+    const refClick = useRef(0);
+
+    const onStateClickHandler = () => {
+        console.log("State Clicked");
+        setStateClick(stateClick + 1);
+    }
+    
+    const onRefClickHandler = () => {
+        console.log("Ref Clicked");
+        refClick.current++;
+    }
+
+  return (
+    <>
+        <div className="text-center mt-3">
+            <h1>State Clicked: {stateClick}</h1>
+            <h1>Ref Clicked: {refClick.current}</h1>
+            <button onClick={onStateClickHandler}>State</button>
+            <button onClick={onRefClickHandler}>Ref</button>
+    </div>
+    </>
+  )
+}
+
+export default StateVsRef
