37e52379b1d7443ebd179ddd073b270972fc6b53
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-11-04 08:03:48 +0530

    used useRef

--- 01_React/19_use_reducer/src/App.jsx
+++ 01_React/19_use_reducer/src/App.jsx
@@ -1,20 +1,54 @@
 import React from "react";
-import { useReducer, useState } from "react";
+import { useReducer, useState, useRef } from "react";
+
+// const counterReducer = (currentState, action) => {
+//   console.log("Paint Reducer", currentState, action);
+
+//   let newState = currentState;
+
+//   if (action.type === "INCREMENT") {
+//     return (newState += 1);
+//   } else if (action.type === "DECREMENT") {
+//     if (newState === 0) {
+//       return newState;
+//     }
+//     return (newState -= 1);
+//   }
+
+//   return newState;
+// };
 
 const counterReducer = (currentState, action) => {
+  console.log("Paint Reducer", currentState, action);
 
   let newState = currentState;
 
-  if(action.type === "INCREMENT"){
-    return newState + 1;
+  switch (action.type) {
+    case "INCREMENT":
+      newState += 1;
+      break;
+    case "DECREMENT":
+      if (newState > 0) {
+        newState -= 1;
+      }
+      break;
+    case "RESET":
+      newState = 0;
+      break;
+    case "DOUBLE":
+      newState *= 2;
+      break;
+    case "changeBy":
+      // newState += parseInt(action.payload.value);
+      newState = parseInt(action.payload.value);
+      break;
+    default:
+      break;
   }
-  else if(action.type === "DECREMENT"){
-    if(newState === 0){
-      return newState;
-    }
-    return newState - 1;
-  }
+
+  return newState;
 };
+
 
 function App() {
   // const [counterValue, setCounterValue] = useState(0);
@@ -31,19 +65,39 @@ function App() {
   //   }
   // }
 
-  const [counterValue, counterDispatch] = useReducer(counterReducer, 0);
+  const initialValue = 0;
+  const [counterValue, counterDispatch] = useReducer(
+    counterReducer,
+    initialValue
+  );
+
+  const changeByInputRef = useRef(null);
 
   const handleIncrement = () => {
-    console.log("Increment Clicked");
+    // console.log("Increment Clicked");
 
     counterDispatch({ type: "INCREMENT" });
   };
 
   const handleDecrement = () => {
-    console.log("Decrement Clicked");
+    // console.log("Decrement Clicked");
 
     counterDispatch({ type: "DECREMENT" });
   };
+
+  const handleChangeByInput = () => {
+
+    const value = changeByInputRef.current.value;
+    changeByInputRef.current.value = "";
+
+    counterDispatch({
+      type: "changeBy",
+      // payload: {
+      //   value: changeByInputRef.current.value,
+      // }
+      payload: { value},
+    })
+  }
 
   return (
     <>
@@ -63,6 +117,25 @@ function App() {
         >
           Decrement
         </button>
+        <button
+          className="bg-blue-500 text-white font-semibold px-3 py-1 border border-blue-900 rounded"
+          onClick={() => counterDispatch({ type: "DOUBLE" })}
+        >
+          Double
+        </button>
+        <button
+          className="bg-blue-500 text-white font-semibold px-3 py-1 border border-blue-900 rounded"
+          onClick={() => counterDispatch({ type: "RESET" })}
+        >
+          Reset
+        </button>
+        <button
+          className="bg-blue-500 text-white font-semibold px-3 py-1 border border-blue-900 rounded"
+          onClick={handleChangeByInput}
+        >
+          Change By
+        </button>
+        <input type="text" placeholder="Enter The Number" id="changeByInput" name="changeByInput" ref={changeByInputRef} className="text-black rounded border border-black px-3"/>
       </div>
     </>
   );
