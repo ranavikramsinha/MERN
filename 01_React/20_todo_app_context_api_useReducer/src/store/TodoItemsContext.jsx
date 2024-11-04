5f8acc6ffa4d5a733cce816d7724a802595712b2
Rana Vikram Sinha <168851099+ranavikramsinha@users.noreply.github.com>
2024-11-04 09:17:13 +0530

    TodoItemsReducer in separate file

--- 01_React/20_todo_app_context_api_useReducer/src/store/TodoItemsContext.jsx
+++ 01_React/20_todo_app_context_api_useReducer/src/store/TodoItemsContext.jsx
@@ -1,34 +1,35 @@
 import { createContext, useReducer, useState } from "react";
 import InitialTodoItems from "./initialTodoItems";
+import TodoItemsReducer from "./TodoItemsReducer";
 
 export const TodoItemsContext = createContext();
 
 // const TodoItemsContext = createContext();
 
-const todoItemsReducer = (currentTodoItems, action) => {
+// const todoItemsReducer = (currentTodoItems, action) => {
 
-  switch(action.type){
-    case 'ADD_ITEM':{
-      // const todoText = action.payload.todoText;
-      // const todoData = action.payload.todoData;
-      const { todoText, todoData } = action.payload;
-      return [...currentTodoItems, {id: currentTodoItems.length + 1, todoText: todoText, todoDate: todoData}];
-    }
-    case 'DELETE_ITEM':{
-      // const id = action.payload.id;
-      const { id } = action.payload;
-      return currentTodoItems.filter(item => item.id !== id);
-    }
-    default:{
-      return currentTodoItems;
-    }
-  }
+//   switch(action.type){
+//     case 'ADD_ITEM':{
+//       // const todoText = action.payload.todoText;
+//       // const todoData = action.payload.todoData;
+//       const { todoText, todoData } = action.payload;
+//       return [...currentTodoItems, {id: currentTodoItems.length + 1, todoText: todoText, todoDate: todoData}];
+//     }
+//     case 'DELETE_ITEM':{
+//       // const id = action.payload.id;
+//       const { id } = action.payload;
+//       return currentTodoItems.filter(item => item.id !== id);
+//     }
+//     default:{
+//       return currentTodoItems;
+//     }
+//   }
 
-}
+// }
 
 export const TodoItemsProvider = ({ children }) => {
 
-    const [todoItems, todoItemsDispatch] = useReducer(todoItemsReducer, InitialTodoItems);
+    const [todoItems, todoItemsDispatch] = useReducer(TodoItemsReducer, InitialTodoItems);
     // console.log(todoItems);
   
     const addTodoItem = (todoText, todoData) => {
---
+++ 01_React/20_todo_app_context_api_useReducer/src/store/TodoItemsReducer.jsx
@@ -0,0 +1,22 @@
+const TodoItemsReducer = (currentTodoItems, action) => {
+
+    switch(action.type){
+      case 'ADD_ITEM':{
+        // const todoText = action.payload.todoText;
+        // const todoData = action.payload.todoData;
+        const { todoText, todoData } = action.payload;
+        return [...currentTodoItems, {id: currentTodoItems.length + 1, todoText: todoText, todoDate: todoData}];
+      }
+      case 'DELETE_ITEM':{
+        // const id = action.payload.id;
+        const { id } = action.payload;
+        return currentTodoItems.filter(item => item.id !== id);
+      }
+      default:{
+        return currentTodoItems;
+      }
+    }
+
+}
+
+export default TodoItemsReducer;
